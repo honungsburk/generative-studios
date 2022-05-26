f23d9b1c067481966f64632998e3176e2f764d5f
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-05-26 14:01:42 +0200

    Add basic routing

---
+++ src/Theme/index.ts
@@ -0,0 +1,42 @@
+import { mode } from "@chakra-ui/theme-tools";
+import { Dict } from "@chakra-ui/utils";
+import { extendTheme } from "@chakra-ui/react";
+import colors from "./Colors";
+
+const global = (props: Dict<any>) => ({
+  body: {
+    color: mode("black", "white")(props),
+    bg: mode("background.light", "background.dark")(props),
+  },
+
+  /* Scrollbar */
+  "::-webkit-scrollbar": {
+    width: "8px",
+  },
+
+  "::-webkit-scrollbar-track": {
+    background: mode("rgba(0, 0, 0, 0.1)", "rgba(255, 255, 255, 0.1)")(props),
+    borderRadius: "40px",
+  },
+
+  "::-webkit-scrollbar-thumb": {
+    borderRadius: "40px",
+    background: mode("rgba(0, 0, 0, 0.4)", "rgba(255, 255, 255, 0.4)")(props),
+  },
+
+  "::-webkit-scrollbar-thumb:hover": {
+    background: mode("rgba(0, 0, 0, 0.7)", "rgba(255, 255, 255, 0.7)")(props),
+  },
+});
+
+export const Theme = extendTheme({
+  styles: {
+    global: global,
+  },
+  components: {},
+  colors: colors,
+  initialColorMode: "light",
+  useSystemColorMode: false,
+});
+
+export default Theme;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Theme from "./Theme";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <ChakraProvider theme={Theme}>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </BrowserRouter>
+);
