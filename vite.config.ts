7d4d4a68bb4e3a77975cad7c9311d2e4a1607121
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-05-27 09:12:22 +0200

    Add debug mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, UserConfigExport } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const baseConfig: UserConfigExport = {
+    plugins: [
+      react({
+        fastRefresh: process.env.NODE_ENV !== "test",
+      }),
+    ],
+  };
+
+  if (mode === "debug") {
+    baseConfig.build = {
+      sourcemap: true,
+      minify: false,
+    };
+  }
+
+  return baseConfig;
 });
