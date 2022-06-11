702e9f0cc15673f35d843d87ecd67aa5ad63db8d
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-06-11 21:11:13 +0200

    Add artlink library

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig, UserConfigExport } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import * as path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const baseConfig: UserConfigExport = {
+    resolve: {
+      alias: {
+        src: path.resolve("src/"),
+      },
+    },
     plugins: [
       react({
         fastRefresh: process.env.NODE_ENV !== "test",
       }),
-      tsconfigPaths(),
     ],
   };
 
